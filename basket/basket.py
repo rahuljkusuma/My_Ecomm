ab4487677026e81cec5da08f26a28fd6cc231f95
rahuljkusuma <59508348+rahuljkusuma@users.noreply.github.com>
2022-01-20 19:51:07 +0530

    v3

--- basket/basket.py
+++ basket/basket.py
@@ -3,14 +3,15 @@
 # s.get_decoded()
 
 
-from store.models import Product
 from decimal import Decimal
+
+from store.models import Product
 
 
 class Basket():
     """
-    A base Basket class, providing some default behaviors that 
-    can be inherited or overrided, as neccessary.
+    A base Basket class, providing some default behaviors that
+    can be inherited or overrided, as necessary.
     """
 
     def __init__(self, request):
@@ -20,18 +21,18 @@ class Basket():
             basket = self.session['skey'] = {}
         self.basket = basket
 
-
     def add(self, product, qty):
         """
         Adding and updating the users basket session data
         """
-        product_id = product.id
+        product_id = str(product.id)
 
-        if product_id not in self.basket:
-            self.basket[product_id] = {'price': float(product.price), 'qty':int(qty)}
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
 
-        self.session.modified = True
-
+        self.save()
 
     def __iter__(self):
         """
@@ -47,9 +48,8 @@ class Basket():
 
         for item in basket.values():
             item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price']*item['qty']
+            item['total_price'] = item['price'] * item['qty']
             yield item
-
 
     def __len__(self):
         """
@@ -57,6 +57,28 @@ class Basket():
         """
         return sum(item['qty'] for item in self.basket.values())
 
+    def update(self, product, qty):
+        """
+        Update values in session data
+        """
+        product_id = str(product)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        self.save()
 
     def get_total_price(self):
-        return sum(Decimal(item['price']*item['qty']) for item in self.basket.values())
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        """
+        Delete item from session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            del self.basket[product_id]
+            print(product_id)
+            self.save()
+
+    def save(self):
+        self.session.modified = True
